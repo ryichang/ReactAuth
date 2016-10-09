18dd09bd1d9d1899c69d570c04ab191684a7ffb9
ryichang <ryichang@gmail.com>
2016-10-08 22:45:12 -0700

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App Setup
+
+// Server Setup
